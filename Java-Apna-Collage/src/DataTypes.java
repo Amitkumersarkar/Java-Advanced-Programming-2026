d157278e11e0f68d3a38df698373a363d3aff87b
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-31 21:37:19 +0600

    added primitive data types

---
+++ Java-Apna-Collage/src/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args){
+//        primitive dataTypes (it's store simple values
+//        byte-1 [-128 to 127]
+//        short-2 bytes
+//        int-4 bytes
+//        long-8 bytes
+//        float-4 bytes [3.5]
+//        double-8 bytes [3.222]
+//        char-2 bytes [a,b,c]
+//        boolean-1 bytes [true/false]
+
+byte age=25;
+int num=123456;
+long num2=12354566987L;
+float pi=3.14F;
+char letter='@';
+boolean isAdult=true;
+        System.out.println(age);
+    }
+}
