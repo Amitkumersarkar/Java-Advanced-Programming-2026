0b8b0137dfa2f004d74215c2287183124b8e58f9
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 01:56:42 +0600

    replace function

---
+++ Java-Apna-Collage/src/Replace.java
@@ -0,0 +1,8 @@
+public class Replace {
+    public static void main(String[] args){
+        String name="amit";
+       String name2= name.replace("amit","amrito");
+        System.out.println(name2);
+    }
+
+}
