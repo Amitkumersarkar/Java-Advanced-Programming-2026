2eac257f77df223bebaf02093997387d4ac628a6
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 01:32:32 +0600

    added concatenate task

---
+++ Java-Apna-Collage/src/Concatenate.java
@@ -0,0 +1,10 @@
+public class Concatenate {
+    public static void main(String[] args){
+    String name1="Amit";
+    String name2="Sarkar";
+    String name3=name1+name2;
+        System.out.println(name3);
+
+    }
+
+}
