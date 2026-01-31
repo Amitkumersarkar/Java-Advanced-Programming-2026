3136e2529a1092b972d9769f3c62c99b2fdc8642
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-31 22:01:28 +0600

    creates extra object

---
+++ Java-Apna-Collage/src/DataTypes2.java
@@ -0,0 +1,11 @@
+public class DataTypes2 {
+//    non primitive data types
+    public static void main(String[] args){
+//        creates extra object
+    String name=new String("AmitSarkar");
+    String friend=new String("Anik");
+        System.out.println(name.length());
+    }
+
+
+}
