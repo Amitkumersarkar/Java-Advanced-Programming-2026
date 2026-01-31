414e1145535869ae1bd8f0b5eb1268522d831a53
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 01:51:17 +0600

    length function in java

--- Java-Apna-Collage/src/Concatenate.java
+++ Java-Apna-Collage/src/Concatenate.java
@@ -4,7 +4,8 @@ public class Concatenate {
     String name2="Sarkar";
     String name3=name1+name2;
         System.out.println(name3);
-
+        System.out.println(name1.charAt(0));
+        System.out.println(name1.length());
     }
 
 }
