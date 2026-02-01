c1f61dfa9f16ec9c2fb32b45afc7ad8efe0bbdf9
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 13:02:43 +0600

    added array length

---
+++ Java-Apna-Collage/src/ArrayLen.java
@@ -0,0 +1,9 @@
+public class ArrayLen {
+    public static void main(String[] args){
+        int physics=90;
+        int chem=85;
+        int[] marks=new int[2];
+        marks[0]=90;
+        System.out.println(marks.length);
+    }
+}
