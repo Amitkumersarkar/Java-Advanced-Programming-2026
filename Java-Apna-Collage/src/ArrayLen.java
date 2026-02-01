3dc7b0fd9aa413cd43cb201dc57ee35827ff304e
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 13:09:52 +0600

    sort in java

--- Java-Apna-Collage/src/ArrayLen.java
+++ Java-Apna-Collage/src/ArrayLen.java
@@ -1,9 +1,17 @@
+import java.util.Arrays;
 public class ArrayLen {
     public static void main(String[] args){
         int physics=90;
         int chem=85;
-        int[] marks=new int[2];
+        int eng=95;
+        int[] marks=new int[3];
         marks[0]=90;
-        System.out.println(marks.length);
+        marks[1]=85;
+        marks[2]=95;
+//        System.out.println(marks.length);
+//        sorts in java
+        System.out.println(marks[0]);
+        Arrays.sort(marks);
+        System.out.println(marks[0]);
     }
 }
