23b904866dbe379de0f13007cb698c9df1d66d79
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 13:13:54 +0600

    added 2D array in java

---
+++ Java-Apna-Collage/src/ArraySorts.java
@@ -0,0 +1,8 @@
+public class ArraySorts {
+    public static void main(String[] args){
+        int[]marks={90,95,85};
+//        2D arrays
+        int [][]finalMarks={{90,95,85},{95,90,85}};
+        System.out.println(finalMarks[0][0]);
+    }
+}
