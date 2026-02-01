048e6fc6b2a48d06c2163f3a58b9d862471a479e
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 14:24:23 +0600

    constants in java

---
+++ Java-Apna-Collage/src/Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+    public static void main(String[] args){
+        int age=26;
+        age=27;
+        age=25;
+      final float PI=3.14F;
+//        PI=1.1F;
+        System.out.println(age);
+    }
+}
