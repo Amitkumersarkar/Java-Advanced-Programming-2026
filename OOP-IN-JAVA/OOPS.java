8371e0bad192819a084f78806f1f6138f5a42ab2
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-02 00:09:57 +0600

    this keyword

--- OOP-IN-JAVA/OOPS.java
+++ OOP-IN-JAVA/OOPS.java
@@ -8,6 +8,11 @@ public void write(){
     System.out.println("writing something");
 
 }
+public void printColor(){
+    // The this keyword in Java refers to the current object in a method or constructor.
+    System.out.println(this.color);
+
+}
 }
 
 public class OOPS {
@@ -16,8 +21,14 @@ public class OOPS {
         Pen p1=new Pen();
         p1.color="Red"; //assign a color
         p1.type="Gel"; //assign type
+
+        //create a new object
+Pen p2=new Pen();
+p2.color="Blue";
+p2.type="BallPoint";
         //here call the function/method
         p1.write();
-
+p1.printColor();
+p2.printColor();
     }
 }
