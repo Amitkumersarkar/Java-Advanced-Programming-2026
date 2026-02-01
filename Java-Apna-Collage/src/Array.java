20569329735166dbc6e428d20a50fe0e9e4ee31e
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-01 12:39:13 +0600

    arrayin java

---
+++ Java-Apna-Collage/src/Array.java
@@ -0,0 +1,18 @@
+public class Array {
+    public static void main(String[] args){
+//        int age=30;
+        int physics=90;
+        int chem=85;
+        int eng=90;
+//        non primitive data type
+int[]marks=new int[3];
+marks[0]=90;
+marks[1]=85;
+marks[2]=90;
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+    }
+}
