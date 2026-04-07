45aed2ed7a2a7a5bf575a30b36cb1328c035bed2
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-04-07 22:07:13 +0600

    integrate swing ui

--- TicTacToe/src/Database.java
+++ TicTacToe/src/Database.java
@@ -1,3 +1,24 @@
+import java.sql.*;
+
 public class Database {
-    
+
+    static final String URL = "jdbc:mysql://localhost:3306/tictactoe";
+    static final String USER = "root";
+    static final String PASS = ""; // your password
+
+    public static void saveResult(String winner) {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASS);
+
+            String query = "INSERT INTO results (winner) VALUES (?)";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, winner);
+
+            ps.executeUpdate();
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
